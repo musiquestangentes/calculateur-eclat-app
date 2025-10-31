b96bc1b9f2052a57e088d348c99cb8dc49d98d1a
musiquestangentes <musiques.tangentes@gmail.com>
2025-11-01 00:14:39 +0100

    Add files via upload

--- app_calculateur.py
+++ app_calculateur.py
@@ -74,7 +74,6 @@ elif module == "Heures lissées":
 
     st.divider()
     st.info("L'équivalent temps plein - ETP - permet de comparer les heures des profs (temps plein fixé à 24h/semaine par la convention collective ECLAT) à un temps plein classique (35h/semaine).")
-    st.write(f"- Heures mensuelles ETP : **{heures_mensuelles_etp:.2f} h**")
     st.markdown("*Formule :*")
     st.latex("\\text{Heures mensuelles ETP} = \\frac{\\text{{Heures hebdo lissées} \\times \\text{151,67}}}{(24)}")
     
@@ -92,6 +91,7 @@ elif module == "Heures lissées":
         st.write(f"- Heures annuelles + 10% CP : **{heures_avec_cp:.2f} h**")
         st.write(f"- Heures mensuelles lissées : **{heures_mensuelles:.2f} h/mois**")
         st.write(f"- Heures hebdomadaires lissées : **{heures_hebdo:.2f} h/semaine**")
+        st.write(f"- Heures mensuelles ETP : **{heures_mensuelles_etp:.2f} h**")
 
 
 # PAGE 3: PRIMES
