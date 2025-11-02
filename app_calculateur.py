aa71c78fe7efa4e3032fefcf5ecb128a3f50f733
musiquestangentes <musiques.tangentes@gmail.com>
2025-11-02 02:32:19 +0100

    Add files via upload

--- app_calculateur.py
+++ app_calculateur.py
@@ -64,7 +64,7 @@ elif module == "Lire sa fiche de paie":
     st.title("Comprendre sa fiche de paie")
 
     svg_code = """
-    <svg viewBox="0 0 1000 1600" width="100%" height="1600" preserveAspectRatio="xMinYMin meet" style="font-family:sans-serif;">
+    <svg viewBox="0 0 1000 1250" width="100%" height="100%" preserveAspectRatio="xMinYMin meet" style="font-family:sans-serif;">
         <style>
             .block { fill:#eef3fd; }
             .text { font-size:13px; }
@@ -285,7 +285,7 @@ elif module == "Lire sa fiche de paie":
     </svg>
     """
 
-    components.html(svg_code, height=1200, scrolling=False)
+    components.html(svg_code, height=900, scrolling=False)
 
 # PAGE 2: COEFFICIENT ET SALAIRE DE BASE
 
