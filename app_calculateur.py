840da511a3fe1afa2722366e3155bd482d34ee17
musiquestangentes <musiques.tangentes@gmail.com>
2025-11-02 03:01:43 +0100

    Add files via upload

--- app_calculateur.py
+++ app_calculateur.py
@@ -365,7 +365,7 @@ elif module == "Heures lissées et ETP":
     st.caption(f"[Lien Légifrance - Temps plein professeur]({url_etp})")
     
     st.divider()
-    st.write("**Calculateur :**")
+    st.write("**Calculateur Heures annuelles réelles -> Heures lissées :**")
     heures_annuelles_reelles = st.number_input(
         "Heures annuelles réellement effectuées (de septembre à août) :", min_value=0.0, step=0.5
     )
@@ -381,6 +381,17 @@ elif module == "Heures lissées et ETP":
         st.write(f"- Heures mensuelles lissées : **{heures_mensuelles:.2f} h/mois**")
         st.write(f"- Heures hebdomadaires lissées : **{heures_hebdo:.2f} h/semaine**")
         st.write(f"- Heures mensuelles ETP : **{heures_mensuelles_etp:.2f} h**")
+
+    st.write("**Calculateur Heures mensuelles ETP -> Heures annuelles réelles :**")
+    heures_mensuelles_etp = st.number_input(
+        "Heures mensuelles ETP (affichées sur fiche de paie) :", min_value=0.0, step=0.5
+    )
+
+    if heures_mensuelles_etp > 0:
+        heures_annuelles_reelles = heures_mensuelles_etp * 7.4805
+
+        st.markdown("### Résultats")
+        st.write(f"- Heures annuelles réelles : **{heures_annuelles_reelles:.2f} h/mois**")
 
 
 # PAGE 4: PRIMES
