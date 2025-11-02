52e7a462cf50247fe6073f2ba7e9a2f20799ab13
musiquestangentes <musiques.tangentes@gmail.com>
2025-11-02 03:36:18 +0100

    Add files via upload

--- app_calculateur.py
+++ app_calculateur.py
@@ -135,10 +135,10 @@ elif module == "Lire sa fiche de paie":
 
         <text x="5.5%" y="315" class="text">Prime d'ancienneté CC 3246</text>
         <text x="55%" y="315" class="text" 
-            onmouseover="showTooltip(evt, 'Nombre d\'années d\'ancienneté × 2')"
+            onmouseover="showTooltip(evt, "Nombre d'années d'ancienneté × 2")"
             onmouseout="hideTooltip()">20</text>
         <text x="65%" y="315" class="text" 
-            onmouseover="showTooltip(evt, 'Taux plus ou moins élevé selon le nombre d\'heures effectuées. Calcul : Prime / Base')"
+            onmouseover="showTooltip(evt, "Taux plus ou moins élevé selon le nombre d'heures effectuées. Calcul : Prime / Base")"
             onmouseout="hideTooltip()">0,9295</text>
         <text x="75%" y="315" class="text"
             onmouseover="showTooltip(evt, 'Calcul : 20 × 0,9295 = 18,59')"
