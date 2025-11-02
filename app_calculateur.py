3db75ac43c41c622fa654bc5164326788108aad1
musiquestangentes <musiques.tangentes@gmail.com>
2025-11-02 02:19:03 +0100

    Add files via upload

--- app_calculateur.py
+++ app_calculateur.py
@@ -64,7 +64,7 @@ elif module == "Lire sa fiche de paie":
     st.title("Comprendre sa fiche de paie")
 
     svg_code = """
-    <svg viewBox="0 0 1000 1500" width="100%" height="auto" preserveAspectRatio="xMinYMin meet" style="font-family:sans-serif;">
+    <svg viewBox="0 0 1000 1600" width="100%" height="1600" preserveAspectRatio="xMinYMin meet" style="font-family:sans-serif;">
         <style>
             .block { fill:#eef3fd; }
             .text { font-size:13px; }
@@ -78,8 +78,8 @@ elif module == "Lire sa fiche de paie":
                 fill-opacity: 1;
                 stroke: #aaa;
                 stroke-width: 0.5;
-                rx: 6;
-                ry: 6;
+                rx: 5;
+                ry: 5;
                 opacity: 1;
             }
             .tooltip-text {
