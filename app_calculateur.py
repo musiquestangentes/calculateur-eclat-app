e1cbd88540cff0daa74b716b2f1825bb1f3f9315
musiquestangentes <musiques.tangentes@gmail.com>
2025-11-03 16:09:28 +0100

    Update app_calculateur.py

--- app_calculateur.py
+++ app_calculateur.py
@@ -320,7 +320,7 @@ elif module == "Lire sa fiche de paie":
         <text x="55%" y="755" class="text"></text>
         <text x="65%" y="755" class="text"></text>
         <text x="75%" y="755" class="text">58,21</text>
-        <text x="85%" y="755" class="text"></text>     
+        <text x="85%" y="755" class="text">49,1</text>     
 
         <!-- Tableau Net à Payer -->
         <rect x="5%" y="770" width="100%" height="30" class="block"/>
